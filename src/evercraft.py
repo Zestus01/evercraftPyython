263426cb91bdfcc9d747cf6ad3e07f08bbd826ca
Zach Estus <zachary.estus@student.awesomeinc.org>
2022-10-25 18:55:20 +0000

    Changed class to dict

--- src/evercraft.py
+++ src/evercraft.py
@@ -1,4 +1,38 @@
+class Character(dict):
+    def __init__(self):
+        self = dict()
+    def create(self, name, alignment):
+        self['name'] = name;
+        self['alignment'] = alignment
+        self['armor'] = 10
+        self['health'] = 5
+        self['is_alive'] = True
+        self['dex'] = 10
+        self['str'] = 10
+        self['con'] = 10
+        self['wis'] = 10
+        self['int'] = 10
+        self['chr'] = 10
+    def get(self, key):
+        return self[key]
+    def add(self, key, value):
+        self[key] = value
+    def attack(self, dice_roll, enemy):
+        if(dice_roll == 20):
+            enemy['health'] -= 2
+            if(enemy['health'] <= 0):
+                enemy['is_alive'] = False
+            return True
+        if(dice_roll >= enemy['armor']):
+            enemy['health'] -= 1
+            if(enemy['health'] <= 0):
+                enemy['is_alive'] = False
+            return True
+        else:
+            return False
+    def modifiers(self, ability):
+        score = self[ability]
+        return score
+        
 
-def x():
-    x = 'five'
-    return x
+    pass
--- src/tests/test_iteration1.py
+++ src/tests/test_iteration1.py
@@ -1,5 +1,64 @@
 from evercraft import *
 
-def test_x_exists():
-    assert x() != 0
-    
+def test_character_exists():
+    assert Character() is not None
+def test_character_name():
+    variable_name = Character()
+    variable_name.create('Sims McBirdman', 'Chaotic Neutral')
+    assert variable_name.get('name') is not None
+def test_set_character_name():
+    name = 'Sims McBirdman'
+    character1 = Character(name, 'Chaotic Neutral')
+    assert character1.name is name
+def test_character2_name():
+    character2 = Character()
+    character2.create('Adfa', 'Neutral Good')
+    assert character2.get('name') is 'Adfa'
+def test_character_change_name():
+    name = 'Sims McBirdman'
+    character1 = Character(name, 'Chaotic Neutral')
+    character1.name = 'Adfa'
+    assert character1.name is not name
+def test_character_alignment():
+    sims = Character("Sims McBirdman", 'Chaotic Neutral')
+    assert sims.alignment is not None
+def test_character_ac_exists():
+    sims = Character("Sims McBirdman", 'Chaotic Neutral')
+    assert sims.armor is not None
+def test_character_health():
+    sims = Character("Sims McBirdman", 'Chaotic Neutral')
+    assert sims.health == 5
+def test_character_attack_hit():
+    sims = Character('Sims McBirdman', 'Chaotic Neutral')
+    rat = Character("rat", "Chaotic Evil")
+    assert sims.attack(12, rat) == True
+def test_character_attack_miss():
+    sims = Character('Sims McBirdman', 'Chaotic Neutral')
+    rat = Character("rat", "Chaotic Evil")
+    assert sims.attack(1, rat) == False
+def test_character_attack_damage():
+    sims = Character('Sims McBirdman', 'Chaotic Neutral')
+    rat = Character("rat", "Chaotic Evil")
+    sims.attack(12, rat)
+    assert rat.health != 5
+def test_character_critical_hit():
+    sims = Character('Sims McBirdman', 'Chaotic Neutral')
+    rat = Character("rat", "Chaotic Evil")
+    sims.attack(20, rat)
+    assert rat.health == 3
+def test_character_isAlive():
+    sims = Character('Sims McBirdman', 'Chaotic Neutral')
+    assert sims.isAlive 
+def test_character_ratDeath():
+    rat = Character('rat', 'Chaotic Neutral')
+    sims = Character('Sims McBirdman', 'Chaotic Neutral')
+    rat.health = 1
+    sims.attack(14, rat)
+    assert not rat.isAlive
+def test_character_abilities():
+    sims = Character('Sims McBirdman', 'Chaotic Neutral')
+    assert sims.dexterity == 10
+def test_character_modifiers():
+    sims = Character('Sims McBirdman', 'Chaotic Neutral')
+    sims.dexterity = 15
+    assert sims.modifiers('dexterity') == 10
