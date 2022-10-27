7d2f260c86684e8ba25f5768a75e084ddfdf3daf
Zach Estus <zachary.estus@student.awesomeinc.org>
2022-10-27 19:01:17 +0000

    Basic Weapon

---
+++ src/tests/test_iteration4.py
@@ -0,0 +1,58 @@
+from evercraft2 import *
+
+def test_is_this_connected():
+    josh = Character('Josh', 'Chaotic Neutral', 'orc')
+    assert josh.race is 'orc'
+
+def test_character_has_weapon():
+    josh = Character('Josh', 'Chaotic Neutral', 'orc')
+    assert josh.weapon_list is not None
+
+def test_character_has_armor():
+    josh = Character('Josh', 'Chaotic Neutral', 'orc')
+    assert josh.armor_list is not None    
+
+def test_character_klass():
+    josh = Fighter('Josh', 'chaotic Neutral')
+    assert josh.klass is "fighter" 
+
+def test_character_can_equip_weapon_two():
+    josh = Fighter('Josh', 'chaotic Neutral')
+    josh.equip_weapon('longsword')
+    assert josh.damage == 5
+
+def test_character_can_equip_weapon():
+    josh = Fighter('Josh', 'chaotic Neutral')
+    josh.equip_weapon('longsword')
+    assert josh.equiped_weapon == 'longsword'   
+
+def test_another_weapon():
+    josh = Fighter('Josh', 'chaotic Neutral')
+    josh.equip_weapon('waraxe')
+    assert josh.damage == 8
+
+def test_if_to_hit_works():
+    josh = Fighter('Josh', 'chaotic Neutral')
+    josh.equip_weapon('waraxe')   
+    assert josh.crit_mult == 3
+
+def test_waraxe_hits():
+    josh = Character('Josh', 'chaotic Neutral')
+    rat = Character('rat', 'Alignment')
+    josh.equip_weapon('waraxe')
+    assert josh.attack(8, rat)
+    
+def test_unequip_weapon():
+    josh = Character('Josh', 'chaotic Neutral')
+    josh.equip_weapon('elven_longsword')
+    assert josh.equiped_weapon is 'elven_longsword'
+
+
+# def test_armor_can_be_equiped():
+#     josh = Character('Josh', 'chaotic Neutral')
+#     josh.equiped_armor()    
+
+
+
+
+
